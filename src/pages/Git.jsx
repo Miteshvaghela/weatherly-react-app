ad072248978251a0fc5228c481dc8201e9a8562f
Miteshvaghela <vaghela_miteshk@yahoo.com>
2024-05-12 19:40:35 +0530

    update

--- src/pages/Git.jsx
+++ src/pages/Git.jsx
@@ -5,7 +5,7 @@ const Git = () => {
     <section className="bg-white dark:bg-gray-900 min-h-10">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
            
-          <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Git Information Page</h1>
+          <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Git Information</h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Introducing our Weather App, your ultimate weather companion! With real-time updates and accurate forecasts, it’s more than just an app - it’s your personal weather guide.</p>
           
       </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
     <section className="bg-white dark:bg-gray-900 min-h-10">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
            
-          <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Services page</h1>
+          <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Services</h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Introducing our Weather App, your ultimate weather companion! With real-time updates and accurate forecasts, it’s more than just an app - it’s your personal weather guide.</p>
           <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
               <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white bg-blue-600 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
